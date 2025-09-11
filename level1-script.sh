4b68936269e288b5de47eac72155fb11cc2adf1e
simonkolz <139152153+simonkolz@users.noreply.github.com>
2025-09-12 00:47:12 +0100

    Create level1-script.sh

---
+++ level1-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Creates a new directory called Arena. (If it doesnt already exists,)
+mkdir -p Arena
+
+# Moves into the Arena directory.
+cd Arena
+
+# Creates three empty files: warrior.txt, mage.txt, and archer.txt
+touch warrior.txt mage.txt archer.txt
+
+# Lists the files in the current directory (which is now Arena).
+ls
