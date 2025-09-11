e40c2e6d68d394e83fe181b6cc81418fb3ec7106
simonkolz <139152153+simonkolz@users.noreply.github.com>
2025-09-12 00:51:33 +0100

    Create level2-script.sh

---
+++ level2-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Number Printing Script
+# ----------------------
+# This script prints numbers from 1 to 10 using a for loop.
+#
+
+# Loop through numbers 1 to 10
+for i in $(seq 1 10); do
+    echo "Number: $i"
+done
